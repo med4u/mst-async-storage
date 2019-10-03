1c92460ba66af3144d50a3ac13e93381a5ef3c25
med4u <med4u@protonmail.com>
2019-10-03 02:04:45 +0200

    fix?

--- src/persist.ts
+++ src/persist.ts
@@ -9,9 +9,9 @@ export async function save(key: string, snapshot: {},cryptoPassword:string) {
 export async function load(key: string,cryptoPassword:string) {
   try {
     const raw = await AsyncStorage.getItem(key)
-    let bytes = CryptoJS.AES.decrypt(raw,cryptoPassword)
-    let data = bytes.toString(CryptoJS.enc.Utf8)
     if (raw) {
+      let bytes = CryptoJS.AES.decrypt(raw,cryptoPassword)
+      let data = bytes.toString(CryptoJS.enc.Utf8)
       return JSON.parse(data)
     }
   } catch {
